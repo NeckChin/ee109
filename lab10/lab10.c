1df6c1dbb8d5955aad7b7e4ad634e2ea4fe42433
Haoda Wang <harry@h313.info>
2018-11-12 18:59:35 -0800

    splash screen, serial partially complete

--- lab10/lab10.c
+++ lab10/lab10.c
@@ -13,13 +13,15 @@
 #include <stdlib.h>
 
 #include "highlow.h"
+#include "serial.h"
 
-int temp = 72;
+short temp = 72;
+short remote = 72;
 
 int main(void) {
   // Set up ports
   DDRB |= (1 << PB4);
-  DDRD |= (1 << PD2) | (1 << PD3);
+  DDRC |= (1 << PC2) | (1 << PC3);
   PORTB |= (1 << PB3) | (1 << PB4);
   PORTC |= (1 << PC5) | (1 << PC1);
   // Set up interrupts
@@ -27,35 +29,53 @@ int main(void) {
   PCICR |= (1 << PCIE0) | (1 << PCIE1);
   PCMSK0 |= (1 << PCINT3) | (1 << PCINT4);
   PCMSK1 |= (1 << PCINT9) | (1 << PCINT13);
+  UCSR0B |= (1 << RXCIE0);
   // Initialize the LCD
   lcd_init();
   lcd_writecommand(1);
   // Initialize encoder
   encoder_init();
-  // Set up LCD
-  lcd_stringout("Temp:   Rmt:");
-  lcd_moveto(1, 0);
+  // Set up serial communication
+  serial_init();
+  // Show splashscreen
+  lcd_moveto(0, 2);
+  lcd_stringout("EE109 Lab 10");
+  lcd_moveto(1, 3);
+  lcd_stringout("Haoda Wang");
+  _delay_ms(500);
+  lcd_writecommand(1);
+  // Reset LCD
   char buf[16];
-  snprintf(buf, 16, "Low=%2d High=%2d  ", get_low_temp(), get_high_temp());
+  lcd_moveto(0, 0);
+  snprintf(buf, 16, "Temp:%2s Rmt:%2s", temp, remote);
+  lcd_stringout(buf);
+  lcd_moveto(1, 0);
+  snprintf(buf, 16, "Low=%2d High=%2d", get_low_temp(), get_high_temp());
   lcd_stringout(buf);
 
   while(1) {
     if(temp < get_low_temp()) {
-      PORTD &= ~(1 << PD2);
-      PORTD |= (1 << PD3);
+      PORTC &= ~(1 << PC2);
+      PORTC |= (1 << PC3);
     }
     else if(temp > get_high_temp()) {
-      PORTD &= ~(1 << PD3);
-      PORTD |= (1 << PD2);
+      PORTC &= ~(1 << PC3);
+      PORTC |= (1 << PC2);
     }
     else {
-      PORTD &= ~((1 << PD3) | (1 << PD2));
+      PORTC &= ~((1 << PC3) | (1 << PC2));
     }
 
     if(temp_has_change()) {
       lcd_moveto(1, 0);
-      snprintf(buf, 16, "Low=%2d High=%2d  ", get_low_temp(), get_high_temp());
+      snprintf(buf, 16, "Low=%2d High=%2d", get_low_temp(), get_high_temp());
       lcd_stringout(buf);
     }
+    if(valid_data()) {
+      lcd_moveto(0, 0);
+      snprintf(buf, 16, "Temp:%2s Rmt:%2s", temp, remote);
+      lcd_stringout(buf);
+    }
+    temp_out(temp);
   }
 }
---
+++ lab10/serial.c
@@ -0,0 +1,82 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+
+#define FOSC 16000000           // Clock frequency
+#define BAUD 9600               // Baud rate used
+#define MYUBRR (FOSC/16/BAUD-1) // Value for UBRR0 register
+
+char buf[5];
+short num_chars = 0;
+char data_valid = 0;
+
+char valid_data() {
+  if(data_valid) {
+    data_valid = 0;
+    return 1;
+  }
+  else
+    return 0;
+}
+
+void temp_out(short temp) {
+  char tem[3];
+  snprintf(tem, 3, "%3s", temp);
+  serial_txchar('@');
+  serial_stringout(tem);
+  serial_txchar('$');
+}
+
+char* remote_temp() {
+  return buf;
+}
+
+void serial_init()
+{
+  UBRR0 = MYUBRR;
+  // Set up USART0 registers
+  UCSR0B |= (1 << TXEN0 | 1 << RXEN0);
+  UCSR0C = (3 << UCSZ00);
+  // Enable tri-state
+  DDRD |= (1 << PD2);
+  PORTD &= ~(1 << PD2);
+  buf[4] = '\0';
+}
+
+void serial_txchar(char ch)
+{
+  while ((UCSR0A & (1<<UDRE0)) == 0);
+  UDR0 = ch;
+}
+
+void serial_stringout(char *s)
+{
+  // Call serial_txchar in loop to send a string
+  for(int i = 0; i < 16; i++) {
+    serial_txchar(s[i]);
+  }
+}
+
+ISR(USART_RX_vect)
+{
+  char ch;
+  // Handle received character
+  ch = UDR0;
+  // Check if data start
+  if(ch == '@') {
+    num_chars = 0;
+  }
+  // Store in buffer
+  else if(ch - '0' < 10 && ch - '0' >= 0) {
+    buf[num_chars++] = ch - '0';
+  }
+  // Check if data end
+  else if(ch == '$') {
+    if(num_chars != 0) {
+      data_valid = 1;
+    }
+  }
+  else {
+    num_chars = 0;
+  }
+}
